33dd5fdda59ee67b66bcdf2f59d867ba26929dd1
Ramiza Imam <ramizaimam6@gmail.com>
2023-09-20 11:29:04 +0700

    Login Page Selesai

--- src/items/LoginCards/index.jsx
+++ src/items/LoginCards/index.jsx
@@ -1,55 +1,58 @@
 import LogoLogin from "../../assets/image/LogoBrand.png";
 import "../../style.css";
-
+import Leaf1 from "../../assets/image/Leaf 1 Login Page.svg"
+import Leaf2 from "../../assets/image/Leaf 2 Login Page.svg" 
 function LoginCard() {
   return (
     <>
+    <img src={Leaf2} className="leaf2" alt="" />
       <div className="container">
-        <div className="d-flex justify-content-center mt-300">
-          <div className="card bg-login" width={550} height={494}>
+        <div className="d-flex justify-content-center mt-100">
+          <div className="card bg-login border-0" width={550} height={494}>
             <div className="pl-24 pt-24 pb-16">
               <img src={LogoLogin} width={152} height={25} alt="..." />
-            </div>
-            <h5 className="pl-24 pr-24">
+            <h5 className="pt-16">
               Indonesia Green and Affordable Housing Program
             </h5>
-            <div className="card-body">
-              <div className="container">
+            <hr  width={502}/>
+            </div>
+            <div className="card-body pl-48 pr-48">
+              <p>Login Akun</p>
                 <form action="">
-                  <div className="container">
-                    <div className="col-12 mt-12">
+                    <div className="mt-12">
                       <input
                         type="email"
                         id="email"
                         name="email"
-                        className="p-email"
+                        className="p-email border-gray"
                         placeholder="Email"
                       />
                     </div>
-                    <div className="col-12 mt-24">
+                    <div className="mt-24">
                       <input
                         type="password"
                         id="password"
                         name="password"
-                        className="p-email"
+                        className="p-email border-gray"
                         placeholder="Password"
                       />
                     </div>
-                  </div>
                 </form>
-                <p className="text-center text-blue pt-16">Lupa Password</p>
-
-                <button className="text-center btn-login">Masuk</button>
-                <div className="">
-                  <hr className="divider-line col-3" />
-                  <p className="col-2 text- atau-color">Atau</p>
-                  <hr className="divider-line col-3" />
-                </div>
-              </div>
-            </div>
-          </div>
+               
+                  
+                <p className="text-center text-blue pt-16 ">Lupa Password</p>                
+                <button className="text-center btn-login text-white border-0">Masuk</button>
+                <div className="d-flex">
+                  <hr className="border-gray" width={206}/>
+                   <div className="divider-text atau-color pl-8 pr-8">Atau</div>
+                  <hr className="border-gray" width={206}/>
+                </div>                  
+                <button className="text-center btn-regis text-green">Registrasi</button>                                                                               
+                </div>              
+                </div>           
         </div>
       </div>
+      <img src={Leaf1} className="leaf1" alt="" />
     </>
   );
 }
