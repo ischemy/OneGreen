81516ed61db3b6a06b0963c0d169c8b9091f6e7d
Ramiza <ramizaimam6@gmail.com>
2023-09-21 21:20:56 +0700

    Rumah Hijau Page Selesai

--- src/items/PaginationItems/index.jsx
+++ src/items/PaginationItems/index.jsx
@@ -1,21 +1,36 @@
-function PaginationItems(){
-    return (
+function PaginationItems() {
+  return (
     <>
-    <div className="d-flex justify-content-center mb-91">
-
-    <div className="pagination">
-  <a href="#" className="green-arrow">&lt;</a>
-  <a href="#" className="text-pagination">1</a>
-  <a className="text-pagination active" href="#">2</a>
-  <a href="#" className="text-pagination">3</a>
-  <a href="#" className="text-pagination">4</a>
-  <a href="#" className="text-pagination">5</a>
-  <a href="#" className="text-pagination">6</a>
-  <a href="#" className="green-arrow">&gt;</a>
-    </div>
-</div>
+      <div className="d-flex justify-content-center mb-91">
+        <div className="pagination">
+          <a href="#" className="green-arrow border-0">
+            &lt;
+          </a>
+          <a href="#" className="text-pagination">
+            1
+          </a>
+          <a className="text-pagination active" href="#">
+            2
+          </a>
+          <a href="#" className="text-pagination">
+            3
+          </a>
+          <a href="#" className="text-pagination">
+            4
+          </a>
+          <a href="#" className="text-pagination">
+            5
+          </a>
+          <a href="#" className="text-pagination">
+            6
+          </a>
+          <a href="#" className="green-arrow border-0">
+            &gt;
+          </a>
+        </div>
+      </div>
     </>
-    )
+  );
 }
 
-export default PaginationItems
+export default PaginationItems;
--- src/items/SearchBar/index.jsx
+++ src/items/SearchBar/index.jsx
@@ -1,16 +1,33 @@
-function SearchBar(){
-    return(
-        <>
-        {/* <div className="container">
+import filter from "../../assets/image/filter.svg";
+import searchIcon from "../../assets/image/search.svg";
+function SearchBar() {
+  return (
+    <>
+      <div className="container mb-40">
         <div className="d-flex justify-content-center">
-
-        <form action="">
-            <input type="text" placeholder="Cari nama perumahan" name="search" className="searchBar br-6 border-search-gray"/>            
-        </form>
+          <div className="wrapper">
+            <input
+              type="text"
+              id="search"
+              name="search"
+              className=""
+              placeholder="Cari nama perumahan"
+            />
+            <button className="">
+              Cari <img src={searchIcon} width={14} height={14} alt="" />
+            </button>
+          </div>
+          <img
+            src={filter}
+            alt=""
+            width={44}
+            height={44}
+            className="mt-13 ml-16"
+          />
         </div>
-        </div> */}
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
-export default SearchBar
+export default SearchBar;
