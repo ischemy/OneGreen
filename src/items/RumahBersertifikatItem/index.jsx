82650785b9b6d4418122d3979d5d15119f08c7fd
Ramiza Imam <ramizaimam6@gmail.com>
2023-09-29 11:13:03 +0700

    tombol next prev

--- src/items/RumahBersertifikatItem/index.jsx
+++ src/items/RumahBersertifikatItem/index.jsx
@@ -5,7 +5,397 @@ import items2 from "../../assets/image/items2.png";
 export function RumahBersertifikatItem() {
   return (
     <>
-      <div className="container mt-80">
+      <section className="pt-5 pb-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-8">
+              <div className="font-judul">RUMAH HIJAU BERSERTIFIKAT</div>
+              <div>
+                <img src={line2} alt="" />
+              </div>
+            </div>
+            <div className="col-4">
+              <a
+                className="btn btn-next-slide mb-3 mr-1"
+                href="#carouselExampleIndicators2"
+                role="button"
+                data-slide="prev"
+              >
+                <i className="fa fa-arrow-left"></i> Lihat Sebelumnya
+              </a>
+              <a
+                className="btn btn-next-slide mb-3 "
+                href="#carouselExampleIndicators2"
+                role="button"
+                data-slide="next"
+              >
+                Lihat Selanjutnya <i className="fa fa-arrow-right"></i>
+              </a>
+            </div>
+            <div className="col-12">
+              <div
+                id="carouselExampleIndicators2"
+                className="carousel slide"
+                data-ride="carousel"
+              >
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <div className="row">
+                      <div className="col-md-4 mb-3">
+                        <div
+                          className="card br-6"
+                          style={{ width: "333px", height: "446px" }}
+                        >
+                          <img
+                            src={items2}
+                            className="card-img-top"
+                            alt="..."
+                            width={333}
+                            height={250}
+                          />
+                          <div className="card-body mb-12">
+                            <div className="text-unit ml-8 mt-12">
+                              18 unit tersedia
+                            </div>
+                            <div className="text-title-card mt-12">
+                              SYIHAB PERSADA I
+                            </div>
+                            <div className="text-address-card">
+                              Kalimantan Selatan, Kab Banjar, Martapura, Cindai
+                              Alus
+                            </div>
+                            <div className="d-flex justify-content-start mt-8 mb-12">
+                              <img
+                                src={logopt2}
+                                alt=""
+                                className="logo-pt-card"
+                              />
+                              <div className="ml-4 text-seller">
+                                SYIHAB PERSADA INDONESIA, PT (REI)
+                              </div>
+                            </div>
+                            <div className="text-blue">BNJ0110072023T001</div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="col-md-4 mb-3">
+                        <div
+                          className="card br-6"
+                          style={{ width: "333px", height: "446px" }}
+                        >
+                          <img
+                            src={items2}
+                            className="card-img-top"
+                            alt="..."
+                            width={333}
+                            height={250}
+                          />
+                          <div className="card-body mb-12">
+                            <div className="text-unit ml-8 mt-12">
+                              18 unit tersedia
+                            </div>
+                            <div className="text-title-card mt-12">
+                              SYIHAB PERSADA I
+                            </div>
+                            <div className="text-address-card">
+                              Kalimantan Selatan, Kab Banjar, Martapura, Cindai
+                              Alus
+                            </div>
+                            <div className="d-flex justify-content-start mt-8 mb-12">
+                              <img
+                                src={logopt2}
+                                alt=""
+                                className="logo-pt-card"
+                              />
+                              <div className="ml-4 text-seller">
+                                SYIHAB PERSADA INDONESIA, PT (REI)
+                              </div>
+                            </div>
+                            <div className="text-blue">BNJ0110072023T001</div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="col-md-4 mb-3">
+                        <div
+                          className="card br-6"
+                          style={{ width: "333px", height: "446px" }}
+                        >
+                          <img
+                            src={items2}
+                            className="card-img-top"
+                            alt="..."
+                            width={333}
+                            height={250}
+                          />
+                          <div className="card-body mb-12">
+                            <div className="text-unit ml-8 mt-12">
+                              18 unit tersedia
+                            </div>
+                            <div className="text-title-card mt-12">
+                              SYIHAB PERSADA I
+                            </div>
+                            <div className="text-address-card">
+                              Kalimantan Selatan, Kab Banjar, Martapura, Cindai
+                              Alus
+                            </div>
+                            <div className="d-flex justify-content-start mt-8 mb-12">
+                              <img
+                                src={logopt2}
+                                alt=""
+                                className="logo-pt-card"
+                              />
+                              <div className="ml-4 text-seller">
+                                SYIHAB PERSADA INDONESIA, PT (REI)
+                              </div>
+                            </div>
+                            <div className="text-blue">BNJ0110072023T001</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="carousel-item">
+                    <div className="row">
+                      <div className="col-md-4 mb-3">
+                        <div
+                          className="card br-6"
+                          style={{ width: "333px", height: "446px" }}
+                        >
+                          <img
+                            src={items2}
+                            className="card-img-top"
+                            alt="..."
+                            width={333}
+                            height={250}
+                          />
+                          <div className="card-body mb-12">
+                            <div className="text-unit ml-8 mt-12">
+                              18 unit tersedia
+                            </div>
+                            <div className="text-title-card mt-12">
+                              SYIHAB PERSADA I
+                            </div>
+                            <div className="text-address-card">
+                              Kalimantan Selatan, Kab Banjar, Martapura, Cindai
+                              Alus
+                            </div>
+                            <div className="d-flex justify-content-start mt-8 mb-12">
+                              <img
+                                src={logopt2}
+                                alt=""
+                                className="logo-pt-card"
+                              />
+                              <div className="ml-4 text-seller">
+                                SYIHAB PERSADA INDONESIA, PT (REI)
+                              </div>
+                            </div>
+                            <div className="text-blue">BNJ0110072023T001</div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="col-md-4 mb-3">
+                        <div
+                          className="card br-6"
+                          style={{ width: "333px", height: "446px" }}
+                        >
+                          <img
+                            src={items2}
+                            className="card-img-top"
+                            alt="..."
+                            width={333}
+                            height={250}
+                          />
+                          <div className="card-body mb-12">
+                            <div className="text-unit ml-8 mt-12">
+                              18 unit tersedia
+                            </div>
+                            <div className="text-title-card mt-12">
+                              SYIHAB PERSADA I
+                            </div>
+                            <div className="text-address-card">
+                              Kalimantan Selatan, Kab Banjar, Martapura, Cindai
+                              Alus
+                            </div>
+                            <div className="d-flex justify-content-start mt-8 mb-12">
+                              <img
+                                src={logopt2}
+                                alt=""
+                                className="logo-pt-card"
+                              />
+                              <div className="ml-4 text-seller">
+                                SYIHAB PERSADA INDONESIA, PT (REI)
+                              </div>
+                            </div>
+                            <div className="text-blue">BNJ0110072023T001</div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="col-md-4 mb-3">
+                        <div
+                          className="card br-6"
+                          style={{ width: "333px", height: "446px" }}
+                        >
+                          <img
+                            src={items2}
+                            className="card-img-top"
+                            alt="..."
+                            width={333}
+                            height={250}
+                          />
+                          <div className="card-body mb-12">
+                            <div className="text-unit ml-8 mt-12">
+                              18 unit tersedia
+                            </div>
+                            <div className="text-title-card mt-12">
+                              SYIHAB PERSADA I
+                            </div>
+                            <div className="text-address-card">
+                              Kalimantan Selatan, Kab Banjar, Martapura, Cindai
+                              Alus
+                            </div>
+                            <div className="d-flex justify-content-start mt-8 mb-12">
+                              <img
+                                src={logopt2}
+                                alt=""
+                                className="logo-pt-card"
+                              />
+                              <div className="ml-4 text-seller">
+                                SYIHAB PERSADA INDONESIA, PT (REI)
+                              </div>
+                            </div>
+                            <div className="text-blue">BNJ0110072023T001</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="carousel-item">
+                    <div className="row">
+                      <div className="col-md-4 mb-3">
+                        <div
+                          className="card br-6"
+                          style={{ width: "333px", height: "446px" }}
+                        >
+                          <img
+                            src={items2}
+                            className="card-img-top"
+                            alt="..."
+                            width={333}
+                            height={250}
+                          />
+                          <div className="card-body mb-12">
+                            <div className="text-unit ml-8 mt-12">
+                              18 unit tersedia
+                            </div>
+                            <div className="text-title-card mt-12">
+                              SYIHAB PERSADA I
+                            </div>
+                            <div className="text-address-card">
+                              Kalimantan Selatan, Kab Banjar, Martapura, Cindai
+                              Alus
+                            </div>
+                            <div className="d-flex justify-content-start mt-8 mb-12">
+                              <img
+                                src={logopt2}
+                                alt=""
+                                className="logo-pt-card"
+                              />
+                              <div className="ml-4 text-seller">
+                                SYIHAB PERSADA INDONESIA, PT (REI)
+                              </div>
+                            </div>
+                            <div className="text-blue">BNJ0110072023T001</div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="col-md-4 mb-3">
+                        <div
+                          className="card br-6"
+                          style={{ width: "333px", height: "446px" }}
+                        >
+                          <img
+                            src={items2}
+                            className="card-img-top"
+                            alt="..."
+                            width={333}
+                            height={250}
+                          />
+                          <div className="card-body mb-12">
+                            <div className="text-unit ml-8 mt-12">
+                              18 unit tersedia
+                            </div>
+                            <div className="text-title-card mt-12">
+                              SYIHAB PERSADA I
+                            </div>
+                            <div className="text-address-card">
+                              Kalimantan Selatan, Kab Banjar, Martapura, Cindai
+                              Alus
+                            </div>
+                            <div className="d-flex justify-content-start mt-8 mb-12">
+                              <img
+                                src={logopt2}
+                                alt=""
+                                className="logo-pt-card"
+                              />
+                              <div className="ml-4 text-seller">
+                                SYIHAB PERSADA INDONESIA, PT (REI)
+                              </div>
+                            </div>
+                            <div className="text-blue">BNJ0110072023T001</div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="col-md-4 mb-3">
+                        <div
+                          className="card br-6"
+                          style={{ width: "333px", height: "446px" }}
+                        >
+                          <img
+                            src={items2}
+                            className="card-img-top"
+                            alt="..."
+                            width={333}
+                            height={250}
+                          />
+                          <div className="card-body mb-12">
+                            <div className="text-unit ml-8 mt-12">
+                              18 unit tersedia
+                            </div>
+                            <div className="text-title-card mt-12">
+                              SYIHAB PERSADA I
+                            </div>
+                            <div className="text-address-card">
+                              Kalimantan Selatan, Kab Banjar, Martapura, Cindai
+                              Alus
+                            </div>
+                            <div className="d-flex justify-content-start mt-8 mb-12">
+                              <img
+                                src={logopt2}
+                                alt=""
+                                className="logo-pt-card"
+                              />
+                              <div className="ml-4 text-seller">
+                                SYIHAB PERSADA INDONESIA, PT (REI)
+                              </div>
+                            </div>
+                            <div className="text-blue">BNJ0110072023T001</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+{
+  /* 
+<div className="container mt-80">
         <div className="font-judul">RUMAH HIJAU BERSERTIFIKAT</div>
         <div>
           <img src={line2} alt="" />
@@ -393,26 +783,5 @@ export function RumahBersertifikatItem() {
             </div>
           </div>
         </div>
-       
-      </div>
-    </>
-  );
-}
-
-{
-  /* <div className="card br-6" style={{width: "333px", height:"446px"}}>
-<img src={items2} className="card-img-top" alt="..." width={333} height={250}/>
-    <div className="card-body mb-12">
-        <div className="text-unit ml-8 mt-12">18 unit tersedia</div>
-       <div className="text-title-card mt-12">SYIHAB PERSADA I</div>
-       <div className="text-address-card">Kalimantan Selatan, Kab Banjar, Martapura, Cindai Alus</div>
-       <div className="d-flex justify-content-start mt-8 mb-12">
-            <img src={logopt2} alt="" className="logo-pt-card"/>
-            <div className="ml-4 text-seller">SYIHAB PERSADA INDONESIA, PT (REI)</div>
-       </div>
-            <div className="text-blue">BNJ0110072023T001</div>
-        
-    </div>
-
-</div> */
+      </div> */
 }
